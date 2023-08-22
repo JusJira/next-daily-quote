6650c9e987a95351302be812fc278677097fec13
JustinJira <jus.jira@gmail.com>
2023-08-22 16:30:48 +0700

    style: change fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import '@/app/globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Gaegu } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const gaegu = Gaegu({ subsets: ['latin'], weight: ['400','700'] })
 
 export const metadata: Metadata = {
   title: 'Quote of The Day - Educational',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={gaegu.className}>{children}</body>
     </html>
   )
 }

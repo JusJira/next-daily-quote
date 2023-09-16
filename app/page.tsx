d91f240e6c1bb49828e095ac4f8d8d4fa1d3f644
JustinJira <jus.jira@gmail.com>
2023-09-16 12:08:58 +0700

    fix: added attribution

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = "edge";
 
 async function getData() {
   const res = await fetch("https://zenquotes.io/api/today");
@@ -15,8 +15,16 @@ export default async function Home() {
     <main className="flex h-[100dvh] flex-col items-center justify-center p-6 md:p-24 bg-neutral-900">
       <blockquote className="font-[700] text-2xl text-white text-center text-balance text">
         {data[0].q}
-        <footer className="font-[400] text-neutral-300 text-base">{data[0].a}</footer>
+        <footer className="font-[400] text-neutral-300 text-base">
+          {data[0].a}
+        </footer>
       </blockquote>
+      <div className="bottom-1 absolute text-neutral-400 text-sm">
+        Quotes API by{" "}
+        <a className='text-blue-100' href="https://zenquotes.io/" target="_blank">
+          ZenQuotes API
+        </a>
+      </div>
     </main>
   );
 }
